b51b521bcddaa8daffff8c055785304b8262a1ef
Luan Nguyen Trong <luannguyentrong98@gmail.com>
2020-01-25 21:03:21 +0700

    color box

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useRef } from 'react';
 import _ from 'lodash';
 import classNames from 'classnames';
 import { Button, Tooltip, Popover } from 'antd';
-import { Editor, EditorState, RichUtils, convertToRaw } from 'draft-js';
+import { Editor, EditorState, RichUtils, Modifier, convertToRaw } from 'draft-js';
 import styles from './App.module.scss';
 
 const ButtonGroup = Button.Group;
@@ -104,6 +104,46 @@ const App = () => {
 		e.preventDefault();
 		handleChangeEditor(RichUtils.toggleBlockType(editorState, blockType));
 	};
+	const handleToggleColor = color => e => {
+		e.preventDefault();
+		const selection = editorState.getSelection();
+
+		// Let's just allow one color at a time. Turn off all active colors.
+		const nextContentState = _.keys(customColorMap)
+		.reduce((contentState, color) => {
+			return Modifier.removeInlineStyle(contentState, selection, color)
+		}, editorState.getCurrentContent());
+
+		let nextEditorState = EditorState.push(
+			editorState,
+			nextContentState,
+			'change-inline-style'
+		);
+
+		const currentStyle = editorState.getCurrentInlineStyle();
+
+		// Unset style override for current color.
+		if (selection.isCollapsed()) {
+			nextEditorState = currentStyle.reduce((state, color) => {
+				return !!customColorMap[color] ? RichUtils.toggleInlineStyle(state, color) : state;
+			}, nextEditorState);
+		}
+
+		// If the color is being toggled on, apply it.
+		if (!currentStyle.has(color)) {
+			nextEditorState = RichUtils.toggleInlineStyle(
+				nextEditorState,
+				color
+			);
+		}
+
+		handleChangeEditor(nextEditorState);
+		saveColorPopoverVisible(false);
+	};
+	const handleVisibleChange = e => {
+		e.preventDefault();
+		saveColorPopoverVisible(!colorPopoverVisible);
+	};
 	const activeStyle = inlineStyle => {
 		const active = editorState.getCurrentInlineStyle().has(inlineStyle);
 		if (active) return activeCSS;
@@ -115,30 +155,21 @@ const App = () => {
 		if (active) return activeCSS;
 		return {}
 	};
-	const getColorContent = () => {
-		const customColorMapKeys = _.keys(customColorMap);
-		let colorData = _.map(customColorMapKeys, colorKey => ({
-			key: colorKey,
-			...customColorMap[colorKey]
-		}));
-		colorData = _.chunk(colorData, 5);
-		return (
-			<div>
-				<div className={styles.header}>Select color</div>
-				<div className={styles.colors}>
-					{_.map(colorData, colorRow => (
-						<div key={_.uniqueId('color_row_')} className={styles.row}>
-							{_.map(colorRow, color => (
-								<span className={classNames(styles.active, styles.color)} key={color.key}>
-									<span className={styles.icon} style={{ backgroundColor: color.color }}/>
-								</span>
-							))}
-						</div>
-					))}
-				</div>
-			</div>
-		)
-	};
+	const customColorMapKeys = _.keys(customColorMap);
+	let colorData = _.map(customColorMapKeys, colorKey => ({
+		key: colorKey,
+		...customColorMap[colorKey]
+	}));
+	colorData = _.chunk(colorData, 5);
+	const currentInlineStyle = editorState.getCurrentInlineStyle();
+	let activeKey;
+	for (let i = 0; i < customColorMapKeys.length; ++i) {
+		if (currentInlineStyle.has(customColorMapKeys[i])) {
+			activeKey = customColorMapKeys[i];
+			break;
+		}
+	}
+	if (!activeKey) activeKey = 'BLACK';
 	//const toggleColorPopoverVisible = () => saveColorPopoverVisible(!colorPopoverVisible);
 	// const handleBeforeInput = (chars, editorState) => {
 	// 	console.log(chars);
@@ -187,13 +218,32 @@ const App = () => {
 							</Tooltip>
 							<Popover
 								placement="bottomLeft"
-								content={getColorContent()}
+								content={(
+									<div>
+										<div className={styles.header}>Select color</div>
+										<div className={styles.colors}>
+											{_.map(colorData, colorRow => (
+												<div key={_.uniqueId('color_row_')} className={styles.row}>
+													{_.map(colorRow, color => (
+														<span 
+															className={activeKey === color.key ? classNames(styles.active, styles.color) : styles.color}
+															key={color.key}
+															onMouseDown={handleToggleColor(color.key)}
+														>
+															<span className={styles.icon} style={{ backgroundColor: color.color }}/>
+														</span>
+													))}
+												</div>
+											))}
+										</div>
+									</div>
+								)}
 								popupClassName={styles.popover}
 								trigger="click"
 								visible={colorPopoverVisible}
-								onVisibleChange={saveColorPopoverVisible}
+								// onVisibleChange={saveColorPopoverVisible}
 							>
-								<Button icon="font-colors" />
+								<Button icon="font-colors" onMouseDown={handleVisibleChange} style={{ color: customColorMap[activeKey].color }}/>
 							</Popover>
 							<Button icon="arrow-up" onClick={handleFocus} />
 						</ButtonGroup>
