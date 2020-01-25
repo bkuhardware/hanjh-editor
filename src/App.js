9884d983ee7233c0fd771a213541bb4b0303b749
Luan Nguyen Trong <luannguyentrong98@gmail.com>
2020-01-25 16:37:51 +0700

    code block

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { useState, useRef } from 'react';
-import { Button } from 'antd';
+import { Button, Tooltip } from 'antd';
 import { Editor, EditorState, RichUtils, convertToRaw } from 'draft-js';
 import styles from './App.module.scss';
 
 const ButtonGroup = Button.Group;
+
+const activeCSS = {
+	color: '#FE7F9C',
+	borderColor: '#FE7F9C',
+	zIndex: 3
+};
 
 const App = () => {
 	const editorRef = useRef(null);
@@ -19,6 +25,12 @@ const App = () => {
 		}
 		return 'not-handled';
 	};
+	const blockStyleFn = contentBlock => {
+		const blockType = contentBlock.getType();
+		if (blockType === 'code-block') {
+			return styles.codeBlock;
+		}
+	}
 	const handleFocus = () => {
 		editorRef.current.focus();
 	};
@@ -26,20 +38,26 @@ const App = () => {
 		e.preventDefault();
 		handleChangeEditor(RichUtils.toggleInlineStyle(editorState, inlineStyle));
 	};
+	const handleBlock = blockType => e => {
+		e.preventDefault();
+		handleChangeEditor(RichUtils.toggleBlockType(editorState, blockType));
+	};
 	const activeStyle = inlineStyle => {
-		const active = currentInlineStyles.has(inlineStyle);
-		if (active) return {
-			color: '#FE7F9C',
-			borderColor: '#FE7F9C',
-			zIndex: 3
-		};
+		const active = editorState.getCurrentInlineStyle().has(inlineStyle);
+		if (active) return activeCSS;
 		return {};
-	}
+	};
+	const activeBlock = blockType => {
+		const selectionState = editorState.getSelection();
+		const active = editorState.getCurrentContent().getBlockForKey(selectionState.getStartKey()).getType() === blockType;
+		if (active) return activeCSS;
+		return {}
+	};
 	// const handleBeforeInput = (chars, editorState) => {
 	// 	console.log(chars);
 	// 	console.log(editorState.getCurrentContent().toJS())
 	// }
-	const currentInlineStyles = editorState.getCurrentInlineStyle();
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.inlineDiv}>
@@ -47,9 +65,21 @@ const App = () => {
 				<div className={styles.main}>
 					<div className={styles.btns}>
 						<ButtonGroup>
-							<Button icon="bold" onMouseDown={hanldeInlineStyle('BOLD')} style={activeStyle('BOLD')}/>
-							<Button icon="italic" onMouseDown={hanldeInlineStyle('ITALIC')} style={activeStyle('ITALIC')} />
-							<Button icon="underline" onMouseDown={hanldeInlineStyle('UNDERLINE')} style={activeStyle('UNDERLINE')}/>
+							<Tooltip placement="bottom" title="Bold">
+								<Button icon="bold" onMouseDown={hanldeInlineStyle('BOLD')} style={activeStyle('BOLD')}></Button>
+							</Tooltip>
+							<Tooltip placement="bottom" title="Italic">
+								<Button icon="italic" onMouseDown={hanldeInlineStyle('ITALIC')} style={activeStyle('ITALIC')} />
+							</Tooltip>
+							<Tooltip placement="bottom" title="Underline">
+								<Button icon="underline" onMouseDown={hanldeInlineStyle('UNDERLINE')} style={activeStyle('UNDERLINE')}/>
+							</Tooltip>
+							<Tooltip placement="bottom" title="Highlight">
+								<Button icon="highlight" />
+							</Tooltip>
+							<Tooltip placement="bottom" title="Code block">
+								<Button icon="code" onMouseDown={handleBlock('code-block')} style={activeBlock('code-block')} />
+							</Tooltip>
 							<Button icon="arrow-up" onClick={handleFocus} />
 						</ButtonGroup>
 					</div>
@@ -60,6 +90,7 @@ const App = () => {
 							onChange={handleChangeEditor}
 							placeholder="Enter content..."
 							handleKeyCommand={handleKeyCommand}
+							blockStyleFn={blockStyleFn}
 							//handleBeforeInput={handleBeforeInput}
 						/>
 					</div>
